c3f9839419e74348e3b75ea6a4588a1375f47b93
Shahar <shaharelys@gmail.com>
2023-06-26 19:31:07 +0300

    debugging..

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ TOTAL_CLASSES_NUMBER = 38  # not in use in final code
 
 # data module configurations
 SEED = 6061993  # the seed for the random split (my birthdate)
-TOTAL_USAGE_RATIO = 5/100  # 40% # not in use in final code
+TOTAL_USAGE_RATIO = 100/100  # 40% # not in use in final code
 TRAIN_RATIO = 0.7*TOTAL_USAGE_RATIO
 VALID_RATIO = 0.15*TOTAL_USAGE_RATIO
 TEST_RATIO = 0.15*TOTAL_USAGE_RATIO
@@ -43,7 +43,7 @@ R_STD, G_STD, B_STD = 0.229, 0.224, 0.225  # ImageNet's RGB std
 
 # train module configurations
 NUM_EPOCH = 10  # 25
-SAVE_INTERVAL_1SNN = 5
+SAVE_INTERVAL_1SNN = 1
 SAVE_INTERVAL_2SNN = 5
 WEIGHT_FILE_PREFIX = "weights_epoch_"
 
