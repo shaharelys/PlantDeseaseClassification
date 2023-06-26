f8c06a7638a5bf45c30749ee3fe2d6ee58abf809
Shahar <shaharelys@gmail.com>
2023-06-26 19:06:10 +0300

    debugging..

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # config.py
 
 # General configurations
-ROOT_DIR = "/content/drive/MyDrive/plant_disease_classification"
-DATA_DIR_OLD = f"{ROOT_DIR}/PlantVillage-Dataset/raw/color/"
-DATA_DIR_1SNN = DATA_DIR_OLD  # f"{ROOT_DIR}/assets/images/PlantVillage-Dataset/raw/organized/"
-DATA_DIR_2SNNS = f"{ROOT_DIR}/assets/PlantVillage-Dataset/raw/2snns/"
+ROOT_DIR = "/content/drive/MyDrive/python_projects/plant_disease_classification"
+DATA_DIR_OLD = f"{ROOT_DIR}/assets/PlantVillage-Dataset/raw/color/"
+DATA_DIR_1SNN = DATA_DIR_OLD  # f"{ROOT_DIR}/assets/PlantVillage-Dataset/raw/organized/"
+DATA_DIR_2SNNS = f"{ROOT_DIR}/assets/PlantVillage-Dataset/raw/color/"
 WEIGHTS_FILE_PATH = f"{ROOT_DIR}/assets/weights"
-WEIGHTS_FILE_PATH_1SNN = f"{WEIGHTS_FILE_PATH}"  # /weights_1snn"
-WEIGHTS_FILE_PATH_2SNNS = f"{WEIGHTS_FILE_PATH}/weights_2snns"
+WEIGHTS_FILE_PATH_1SNN = f"{WEIGHTS_FILE_PATH}/1snn"
+WEIGHTS_FILE_PATH_2SNNS = f"{WEIGHTS_FILE_PATH}/2snns"
 
 # model module configurations
 PLANT_CLASSES = {
@@ -32,7 +32,7 @@ TOTAL_CLASSES_NUMBER = 38  # not in use in final code
 
 # data module configurations
 SEED = 6061993  # the seed for the random split (my birthdate)
-TOTAL_USAGE_RATIO = 100/100  # 40% # not in use in final code
+TOTAL_USAGE_RATIO = 5/100  # 40% # not in use in final code
 TRAIN_RATIO = 0.7*TOTAL_USAGE_RATIO
 VALID_RATIO = 0.15*TOTAL_USAGE_RATIO
 TEST_RATIO = 0.15*TOTAL_USAGE_RATIO
@@ -42,7 +42,7 @@ R_MEAN, G_MEAN, B_MEAN = 0.485, 0.456, 0.406  # ImageNet's RGB means
 R_STD, G_STD, B_STD = 0.229, 0.224, 0.225  # ImageNet's RGB std
 
 # train module configurations
-NUM_EPOCH = 20  # 25
+NUM_EPOCH = 10  # 25
 SAVE_INTERVAL_1SNN = 5
 SAVE_INTERVAL_2SNN = 5
 WEIGHT_FILE_PREFIX = "weights_epoch_"
